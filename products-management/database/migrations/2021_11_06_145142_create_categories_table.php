5cd076175fc67e6ce458748a16ddee28bd2644ea
ismailouchaou <ismailouchaou.pro@gmail.com>
2021-11-07 01:53:26 +0100

    create categories table

---
+++ products-management/database/migrations/2021_11_06_145142_create_categories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string("name");
+
+            $table->timestamps();
+
+            //foreign Key
+            $table->foreignId('parent_category')
+                    ->constrained('categories','id')
+                    ->nullable()
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
